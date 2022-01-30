7a3d52ac1d2fd0d05c2a2c01284443b2a2384564
Emerson Dias de Oliveira <emersondiaspd@gmail.com>
2022-01-30 14:27:38 -0300

    Merge

---
+++ usuarios-service/src/main/resources/db/migration/V2__Populate_Table_Usuarios.sql
@@ -0,0 +1,14 @@
+ INSERT INTO `usuarios` (`nome`,`sobre_nome`,`email`, `login`,`telefone`,`senha`,`situacao`, `codigo_ativar`,  `data_limite_ativar` ) VALUES 
+                        ('Emerson', 'Dias de Oliveira', 'emersondiaspd@gmail.com', '11988298946', '11988298946', '$2a$10$ni/paTq3aKJpIFnTSZDnCOasamoFaY.bx3orFQrhK11owDss2seNy', 2, null, null);
+
+      INSERT INTO `usuarios` (`nome`,`sobre_nome`,`email`, `login`,`telefone`,`senha`,`situacao`, `codigo_ativar`,  `data_limite_ativar` ) VALUES 
+                        ('Marco', 'santos', 'emailzinho@teste.com', '11123456789', '11123456789', '$2a$10$ni/paTq3aKJpIFnTSZDnCOasamoFaY.bx3orFQrhK11owDss2seNy', 1, 1234, null);
+
+                       	
+      INSERT INTO `usuarios` (`nome`,`sobre_nome`,`email`, `login`,`telefone`,`senha`,`situacao`, `codigo_ativar`,  `data_limite_ativar` ) VALUES 
+                        ('Relampago', 'Marquinho', 'emailzinho@teste.com', '12345678901', '12345678901', '$2a$10$ni/paTq3aKJpIFnTSZDnCOasamoFaY.bx3orFQrhK11owDss2seNy', 1, 1234, null);
+                        
+                              INSERT INTO `usuarios` (`nome`,`sobre_nome`,`email`, `login`,`telefone`,`senha`,`situacao`, `codigo_ativar`,  `data_limite_ativar` ) VALUES 
+                        ('Joao', 'NEes', 'emailzinho@teste.com', '11223344556', '11223344556', '$2a$10$ni/paTq3aKJpIFnTSZDnCOasamoFaY.bx3orFQrhK11owDss2seNy', 2, null, null);
+
+                       	                   	                    	 
